b26d0eae93f982064ec9c8da68b7d6894af797f9
I-Maged <magedhanafy@gmail.com>
2026-04-26 20:05:17 +0300

    setting up postgres connection

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,20 +12,34 @@ const port = 8080
 
 type application struct {
 	Domain string
+	DSN    string
+	DB     *sql.DB
 }
 
 func main() {
 	// set application config
 	var app application
+
 	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=moviesDB sslmode=disable timezone=UTC connect_timeout=5", "postgres connection string")
+
+	flag.Parse()
 
 	// connect to the datebase
+	conn, err := app.connectToDB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
+
 	app.Domain = "example.com"
 
 	// start a web server
 	log.Println("Starting server on port: ", port)
 
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
