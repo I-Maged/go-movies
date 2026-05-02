48395acca738528d72d8e13dd7e4bf9fc790e809
I-Maged <magedhanafy@gmail.com>
2026-05-02 19:15:11 +0300

    creating authentication handler

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -18,6 +18,8 @@ func (app *application) routes() http.Handler {
 	// routes
 	mux.Get("/", app.Home)
 
+	mux.Get("/authenticate", app.authenticate)
+
 	mux.Get("/movies", app.AllMovies)
 
 	return mux
