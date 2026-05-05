810b6291ae2c3f3a5b218a4d69c44ed650b6c25c
I-Maged <magedhanafy@gmail.com>
2026-05-05 21:40:50 +0300

    frontend authorization

--- client/src/components/ManageCatalouge.jsx
+++ client/src/components/ManageCatalouge.jsx
@@ -1,8 +1,58 @@
+import { useEffect, useState } from 'react'
+import { Link, useNavigate, useOutletContext } from 'react-router-dom'
+
 const ManageCatalouge = () => {
+  const [movies, setMovies] = useState([])
+  const { jwtToken } = useOutletContext()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!jwtToken) {
+      navigate('/login')
+      return
+    }
+    function getMovies() {
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+      headers.append('Authorization', 'Bearer ' + jwtToken)
+
+      const requestOptions = { method: 'GET', headers: headers }
+
+      fetch(`/api/admin/movies`, requestOptions)
+        .then((response) => response.json())
+        .then((data) => {
+          setMovies(data)
+        })
+        .catch((err) => console.log(err))
+    }
+
+    getMovies()
+  }, [jwtToken, navigate])
+
   return (
     <div className='text-center'>
-      <h2>Manage Catalouge</h2>
+      <h2>Manage Catalogue</h2>
       <hr />
+      <table className='table table-striped table-hover'>
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <tr key={movie.id}>
+              <th>
+                <Link to={`/api/admin/movies/${movie.id}`}>{movie.title}</Link>
+              </th>
+              <th>{movie.release_date}</th>
+              <th>{movie.mpaa_rating}</th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
