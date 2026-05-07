10985e06fb079cd4aa204f86c68fe7c99a79d1f7
I-Maged <magedhanafy@gmail.com>
2026-05-07 18:06:40 +0300

    edit movie fields

--- client/src/components/EditMovie.jsx
+++ client/src/components/EditMovie.jsx
@@ -1,6 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useOutletContext, useParams } from 'react-router-dom'
 import Input from './form/Input'
+import Select from './form/Select'
+import TextArea from './form/TextArea'
+
+/* const genreOptions = [
+  { id: 'Drama', value: 'Drama' },
+  { id: 'Crime', value: 'Crime' },
+  { id: 'Action', value: 'Action' },
+  { id: 'Comic Book', value: 'Comic Book' },
+  { id: 'Sci-Fi', value: 'Sci-Fi' },
+  { id: 'Mystery', value: 'Mystery' },
+  { id: 'Adventure', value: 'Adventure' },
+  { id: 'Comedy', value: 'Comedy' },
+  { id: 'Romance', value: 'Romance' },
+] */
+const mpaaOptions = [
+  { id: 'G', value: 'G' },
+  { id: 'PG', value: 'PG' },
+  { id: 'PG-13', value: 'PG-13' },
+  { id: 'R', value: 'R' },
+  { id: 'NC17', value: 'NC17' },
+  { id: '18A', value: '18A' },
+]
 
 const EditMovie = () => {
   const { jwtToken } = useOutletContext()
@@ -40,21 +62,65 @@ const EditMovie = () => {
   }
 
   return (
-    <div className='text-center'>
+    <div>
       <h2>Add/Edit Movie</h2>
       <hr />
+
+      <pre>{JSON.stringify(movie, null, 3)}</pre>
+
       <form onSubmit={handleSubmit}>
         <input type='hidden' name='id' value={movie.id} id='id' />
-
         <Input
           title={'title'}
           name={'title'}
           className={'form-control'}
           type={'text'}
           value={movie.title}
-          onChange={handleChange('title')}
+          onChange={handleChange}
           errorDiv={hasError('title') ? 'text-danger' : 'd-none'}
           errorMsg={'Please enter a title'}
+        />
+        <Input
+          title={'Release Date'}
+          name={'release_date'}
+          className={'form-control'}
+          type={'date'}
+          value={movie.release_date}
+          onChange={handleChange}
+          errorDiv={hasError('release_date') ? 'text-danger' : 'd-none'}
+          errorMsg={'Please enter a Release Date'}
+        />
+        <Input
+          title={'Runtime'}
+          name={'runtime'}
+          className={'form-control'}
+          type={'text'}
+          value={movie.runtime}
+          onChange={handleChange}
+          errorDiv={hasError('runtime') ? 'text-danger' : 'd-none'}
+          errorMsg={'Please enter Runtime'}
+        />
+        <Select
+          title={'MPAA Rating'}
+          name={'mpaa_rating'}
+          value={movie.runtime}
+          onChange={handleChange}
+          placeHolder={'Choose rating'}
+          options={mpaaOptions}
+          errorDiv={hasError('mpaa_rating') ? 'text-danger' : 'd-none'}
+          errorMsg={'Please enter a rating'}
+        />
+
+        <TextArea
+          title={'Description'}
+          name={'description'}
+          className={'form-control'}
+          type={'text'}
+          value={movie.description}
+          rows={'3'}
+          onChange={handleChange}
+          errorDiv={hasError('description') ? 'text-danger' : 'd-none'}
+          errorMsg={'Please enter a description'}
         />
       </form>
     </div>
--- client/src/components/form/Select.jsx
+++ client/src/components/form/Select.jsx
@@ -12,16 +12,14 @@ const Select = (props) => {
         value={props.value}
         onChange={props.onChange}
       >
-        <option value=''>
-          {props.placeHolder}
-          {props.options.map((option) => {
-            return (
-              <option key={option.id} value={option.id}>
-                {option.value}
-              </option>
-            )
-          })}
-        </option>
+        {props.placeHolder}
+        {props.options.map((option) => {
+          return (
+            <option key={option.id} value={option.id}>
+              {option.value}
+            </option>
+          )
+        })}
       </select>
       <div className={props.errorDiv}>{props.errorMsg}</div>
     </div>
