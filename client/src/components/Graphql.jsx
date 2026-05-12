6bd68c6cce3763d717795171713c5c9260b8bf86
I-Maged <magedhanafy@gmail.com>
2026-05-12 04:31:08 +0300

    get movies - frontend graphql

--- client/src/components/Graphql.jsx
+++ client/src/components/Graphql.jsx
@@ -1,8 +1,88 @@
+import { useEffect, useState } from 'react'
+import Input from './form/Input'
+import { Link } from 'react-router-dom'
+
 const Graphql = () => {
+  const [movies, setMovies] = useState([])
+  const [fullList, setFullList] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  // const searchTermRef = useRef()
+
+  const handleChange = (e) => {
+    e.preventDefault()
+  }
+  const performSearch = () => {}
+
+  useEffect(() => {
+    const payload = `
+                  {
+                    list {
+                        id
+                        title
+                        runtime
+                        release_date
+                        mpaa_rating
+                    }
+                  }`
+
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/graphql')
+
+    const requestOptions = { method: 'POST', headers: headers, body: payload }
+
+    fetch(`/api/graph`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        let theList = Object.values(data.data.list)
+        setMovies(theList)
+        setFullList(theList)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
   return (
     <div className='text-center'>
       <h2>GraphQL</h2>
       <hr />
+      <form onSubmit={handleChange}>
+        <Input
+          title='Search Term'
+          type='search'
+          className='form-control'
+          id='searchTerm'
+          name='searchTerm'
+          // ref={searchTermRef}
+          placeholder='Search Term'
+          onChange={handleChange}
+          autoComplete='search-term-new'
+          value={searchTerm}
+        />
+      </form>
+
+      {movies ? (
+        <table className='table table-striped table-hover'>
+          <thead>
+            <tr>
+              <th>Movie</th>
+              <th>Release Date</th>
+              <th>Rating</th>
+            </tr>
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie.id}>
+                <th>
+                  <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                </th>
+                <th>{new Date(movie.release_date).toLocaleDateString()}</th>
+                <th>{movie.mpaa_rating}</th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No movies yet</p>
+      )}
     </div>
   )
 }
--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -28,7 +28,7 @@ func (app *application) routes() http.Handler {
 	mux.Get("/genres", app.GetAllGenres)
 	mux.Get("/movies/genres/{id}", app.AllMoviesByGenre)
 
-	mux.Get("/graph", app.MoviesGraphQL)
+	mux.Post("/graph", app.MoviesGraphQL)
 
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(app.authRequired)
